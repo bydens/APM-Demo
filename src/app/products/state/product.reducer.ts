4150f8dbb16f5c8d1043f16b8f569b7a450f4ee9
Denys Bykanov <bydens@gmail.com>
2018-11-11 20:52:06 +0200

    interfaces slectors

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,5 +1,41 @@
+import { Product } from "../product";
+import * as fromRoot from '../../state/app.state';
+import { createFeatureSelector, createSelector } from "@ngrx/store";
 
-export function reducer(state, action) {
+export interface State extends fromRoot.State {
+  product: ProductState;
+}
+
+export interface ProductState {
+  showProductCode: boolean;
+  currentProduct: Product;
+  products: Product[];
+}
+
+const initialState: ProductState = {
+  showProductCode: true,
+  currentProduct: null,
+  products: []
+}
+
+const getProductFeaturesState = createFeatureSelector<ProductState>('products');
+
+export const getShowProductCode = createSelector(
+  getProductFeaturesState,
+  state => state.showProductCode
+);
+
+export const getCurrentPoduct = createSelector(
+  getProductFeaturesState,
+  state => state.currentProduct
+);
+
+export const getProducts = createSelector(
+  getProductFeaturesState,
+  state => state.products
+);
+
+export function reducer(state = initialState, action): ProductState {
   switch (action.type) {
     case 'TOGGLE_PRODUCT_CODE':
       return {
